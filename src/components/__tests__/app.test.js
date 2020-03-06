e8239679ba3e434015efed063f21f2f5153e2e56
LTUC Student 1 <std1@QUDSLABS.LOCAL>
2020-03-06 09:59:59 +0200

    fixing eslint problems

--- src/components/__tests__/app.test.js
+++ src/components/__tests__/app.test.js
@@ -1,22 +1,23 @@
-import React from 'react';
+// eslint-disable-line no-unused-vars
+import React from 'react';// eslint-disable-line no-unused-vars
 import { shallow , mount } from 'enzyme';
 import renderer from 'react-test-renderer';
-import Counter from '../counter/counter.js';
+import Counter from '../counter/counter.js';// eslint-disable-line no-unused-vars
 
 describe('<Counter/>' , ()=>{
-    it('exists at the start of our application',()=>{
-      let app = shallow(<Counter />);
-      expect(app.find('section').exists()).toBeTruthy();  
-    });
-    it('change the state on click',()=>{
-      let app = mount(<Counter />);
-      let button = app.find('.up');
-      button.simulate('click');
-      expect(app.state('count')).toBe(1);
+  it('exists at the start of our application',()=>{
+    let app = shallow(<Counter />);
+    expect(app.find('section').exists()).toBeTruthy();  
+  });
+  it('change the state on click',()=>{
+    let app = mount(<Counter />);
+    let button = app.find('.up');
+    button.simulate('click');
+    expect(app.state('count')).toBe(1);
 
-    });
-    it('renders correctly',()=> {
-const rendersTree = renderer.create(<Counter />).toJSON();
-expect(rendersTree).toMatchSnapshot();
-    })
-})
+  });
+  it('renders correctly',()=> {
+    const rendersTree = renderer.create(<Counter />).toJSON();
+    expect(rendersTree).toMatchSnapshot();
+  });
+});
